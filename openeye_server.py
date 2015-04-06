1d3358cf86a79702e4e521bb13391f91951a6ad5
johnespinosa <johncespinosa@gmail.com>
2015-04-06 09:46:25 +0200

    Decoupled server from db interactions

--- openeye_server.py
+++ openeye_server.py
@@ -1,31 +1,19 @@
 import solr_interaction
 from solr_interaction import solr_instance
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-    render_template, flash, render_template, json, jsonify, request
-import flask
-from flask.helpers import make_response
-import flask.views
-from flask.wrappers import Request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
-def homepage_endpoint():
+def hello_world():
     return render_template('temp.html') 
 
-def add_similar_articles(results):
-    for result in results:
-        related_articles = solr_interaction.get_similar_articles(int(result['id']), solr_instance)
-        result['related'] = related_articles
-
 @app.route('/search',  methods=['GET', 'POST'])
-def search_endpoint():
+def search():
     query_string = request.form['search_input']
     results = solr_interaction.search_query(query_string, solr_instance)
-    add_similar_articles(results)
     return render_template('temp.html', results = results)
     
 app.debug = True
 app.run()
-    
